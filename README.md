152156641cd9907c03822bd03f34d9e905f8ceaa
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2019-07-13 13:09:14 +0200

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# dile-rounded-button
-Web Component to implement a button with rounded corners.
+# \<dile-rounded-button>
+
+Web component to implement a styled button, with background color and rounded corners. Based on LitElement.
+
+## Properties
+
+- **disabled**: Boolean property to make the button disabled.
+
+## Styles
+
+You can customize it using CSS Custom Properties.
+
+Custom property | Description | Default
+----------------|-------------|---------
+--dile-rounded-button-height | Height of the button | 30px
+--dile-rounded-button-background-color | Button background color | #e74c3c
+--dile-rounded-button-text-color | Button text color | #fff
+--dile-rounded-button-hover-background-color | Button hover background color | #303030
+--dile-rounded-button-hover-text-color | Button hover text color | #fff
+--dile-rounded-button-animation-time | Hover button color animation | 0.3s
+
+## Events
+
+This component dispatch a custom event when the user clicks the button
+
+- **dile-rounded-button-click**: there was a click in the button. If the button is in disabled state, the component do not dispatch the event.
---
+++ dile-rounded-button.js
@@ -0,0 +1,72 @@
+import { LitElement, html, css } from 'lit-element';
+
+class EitRoundedButton  extends LitElement {
+
+  static get styles() {
+    return css`
+      :host {
+        display: inline-block;
+      }
+      button {
+        border-color: transparent;
+        border-radius: 30px;
+        border-style: solid;
+        border-width: 1px;
+        box-sizing: inherit;
+        cursor: pointer;
+        display: inline-block;
+        line-height: normal;
+        margin: 0;
+        padding: 5px 20px;
+        position: relative;
+        text-decoration: none;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        border-radius: var(--dile-rounded-button-height, 30px);
+        font-size: 12px;
+        text-transform: uppercase;
+        height: var(--dile-rounded-button-height, 30px);
+        text-align: center;
+        transition: all var(--dile-rounded-button-animation-time, 0.3s) ease;
+        background-color: var(--dile-rounded-button-background-color, #e74c3c);
+        color: var(--dile-rounded-button-text-color, #fff);
+      }
+      button:hover {
+        background-color: var(--dile-rounded-button-hover-background-color, #303030);
+        color: var(--dile-rounded-button-hover-text-color, #fff);
+      }
+      button[disabled] {
+        cursor: default;
+        opacity: 0.6;
+        background-color: var(--dile-rounded-button-background-color, #e74c3c);
+        color: var(--dile-rounded-button-text-color, #fff);
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      disabled: { type: Boolean },
+    };
+  }
+
+  constructor() {
+    super();
+    this.disabled = false;
+  }
+
+  render() {
+    return html`
+      <button @click="${this.onClick}" ?disabled="${this.disabled}"><slot></slot></button>
+    `;
+  }
+
+  onClick() {
+    console.log('click');
+    this.dispatchEvent(new CustomEvent('dile-rounded-button-click'));
+  }
+}
+
+customElements.define('dile-rounded-button', EitRoundedButton);
